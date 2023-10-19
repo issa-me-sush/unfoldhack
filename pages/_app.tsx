4d98f392254a0ef70706750635e2e0f9ff501175
issa-me-sush <coolsushaan@gmail.com>
2023-10-20 03:52:05 +0530

    initial commit - okto not working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { getDefaultWallets, RainbowKitProvider ,connectorsForWallets} from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { OktoConnector } from '@okto_wallet/okto-connect-sdk';
+
 import {
   arbitrum,
   goerli,
@@ -13,7 +15,7 @@ import {
   zora,
 } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
-
+const projectId = "bbc302f89a722c69f043215565e5bf08"
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
     mainnet,
@@ -27,12 +29,35 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
   [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+// const { connectors } = getDefaultWallets({
+//   appName: 'RainbowKit App',
+//   projectId,
+//   chains,
+// });
+const oktoConnector = new OktoConnector({
+  chains,
+  options: {
+    projectId: "bbc302f89a722c69f043215565e5bf08",
+  },
+});
+const { wallets } = getDefaultWallets({
+  appName: "RainbowKit demo",
+  projectId,
   chains,
 });
+const connectors = connectorsForWallets([
+  
+  ...wallets,
+  {
+    groupName: "Okto",
+    wallets: [
+   
+      
+     
+    ],
+  },
 
+]);
 const wagmiConfig = createConfig({
   autoConnect: true,
   connectors,
