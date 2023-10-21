a2037c1a77555bf29677c2f87ccf997bf8d5b407
Aadinir <aadiniranjan27@gmail.com>
2023-10-21 09:37:30 +0530

    final commit

--- contracts/abi.js
+++ contracts/abi.js
@@ -342,279 +342,351 @@ export const PRIVABI=
 	}
 ]
 
-export const PUBCONTRACT = "0xe248aD9f74342423ee5c097874B9A253691E2Ad6"
+export const PUBCONTRACT = "0x901303FC0a875B2B8149aBF82C79B56919FD61Ee"
 
-export const PUBABI=
-
-[
-{
-"inputs": [
-{
-"internalType": "uint256",
-"name": "_entryFee",
-"type": "uint256"
-},
-{
-"internalType": "uint256",
-"name": "_potSize",
-"type": "uint256"
-}
-],
-"name": "createPublicEnvelope",
-"outputs": [],
-"stateMutability": "nonpayable",
-"type": "function"
-},
-{
-"inputs": [],
-"name": "deposit",
-"outputs": [],
-"stateMutability": "payable",
-"type": "function"
-},
-{
-"inputs": [
-{
-"internalType": "uint256",
-"name": "envelopeId",
-"type": "uint256"
-}
-],
-"name": "distributeRewards",
-"outputs": [],
-"stateMutability": "nonpayable",
-"type": "function"
-},
-{
-"anonymous": false,
-"inputs": [
-{
-"indexed": false,
-"internalType": "uint256",
-"name": "envelopeId",
-"type": "uint256"
-},
-{
-"indexed": false,
-"internalType": "address",
-"name": "participant",
-"type": "address"
-}
-],
-"name": "EnteredEnvelope",
-"type": "event"
-},
-{
-"inputs": [
-{
-"internalType": "uint256",
-"name": "envelopeId",
-"type": "uint256"
-}
-],
-"name": "enterEnvelope",
-"outputs": [],
-"stateMutability": "payable",
-"type": "function"
-},
-{
-"anonymous": false,
-"inputs": [
-{
-"indexed": false,
-"internalType": "address",
-"name": "user",
-"type": "address"
-},
-{
-"indexed": false,
-"internalType": "uint256",
-"name": "amount",
-"type": "uint256"
-}
-],
-"name": "FundAdded",
-"type": "event"
-},
-{
-"anonymous": false,
-"inputs": [
-{
-"indexed": false,
-"internalType": "address",
-"name": "user",
-"type": "address"
-},
-{
-"indexed": false,
-"internalType": "uint256",
-"name": "amount",
-"type": "uint256"
-}
-],
-"name": "FundsWithdrawn",
-"type": "event"
-},
-{
-"anonymous": false,
-"inputs": [
-{
-"indexed": false,
-"internalType": "uint256",
-"name": "envelopeId",
-"type": "uint256"
-},
-{
-"indexed": false,
-"internalType": "address",
-"name": "creator",
-"type": "address"
-},
-{
-"indexed": false,
-"internalType": "uint256",
-"name": "entryFee",
-"type": "uint256"
-}
-],
-"name": "PublicEnvelopeCreated",
-"type": "event"
-},
-{
-"anonymous": false,
-"inputs": [
-{
-"indexed": false,
-"internalType": "uint256",
-"name": "envelopeId",
-"type": "uint256"
-}
-],
-"name": "PublicEnvelopeDistributed",
-"type": "event"
-},
-{
-"inputs": [
-{
-"internalType": "uint256",
-"name": "amount",
-"type": "uint256"
-}
-],
-"name": "withdraw",
-"outputs": [],
-"stateMutability": "nonpayable",
-"type": "function"
-},
-{
-"inputs": [],
-"name": "CREATOR_REWARD_PERCENTAGE",
-"outputs": [
-{
-"internalType": "uint256",
-"name": "",
-"type": "uint256"
-}
-],
-"stateMutability": "view",
-"type": "function"
-},
-{
-"inputs": [],
-"name": "currentEnvelopeId",
-"outputs": [
-{
-"internalType": "uint256",
-"name": "",
-"type": "uint256"
-}
-],
-"stateMutability": "view",
-"type": "function"
-},
-{
-"inputs": [],
-"name": "FEE_PERCENTAGE",
-"outputs": [
-{
-"internalType": "uint256",
-"name": "",
-"type": "uint256"
-}
-],
-"stateMutability": "view",
-"type": "function"
-},
-{
-"inputs": [
-{
-"internalType": "address",
-"name": "",
-"type": "address"
-}
-],
-"name": "loyaltyPoints",
-"outputs": [
-{
-"internalType": "uint256",
-"name": "",
-"type": "uint256"
-}
-],
-"stateMutability": "view",
-"type": "function"
-},
-{
-"inputs": [
-{
-"internalType": "uint256",
-"name": "",
-"type": "uint256"
-}
-],
-"name": "publicEnvelopes",
-"outputs": [
-{
-"internalType": "address",
-"name": "creator",
-"type": "address"
-},
-{
-"internalType": "uint256",
-"name": "potSize",
-"type": "uint256"
-},
-{
-"internalType": "uint256",
-"name": "entryFee",
-"type": "uint256"
-},
-{
-"internalType": "uint256",
-"name": "creatorReward",
-"type": "uint256"
-}
-],
-"stateMutability": "view",
-"type": "function"
-},
-{
-"inputs": [
-{
-"internalType": "address",
-"name": "",
-"type": "address"
-}
-],
-"name": "userBalances",
-"outputs": [
-{
-"internalType": "uint256",
-"name": "",
-"type": "uint256"
-}
-],
-"stateMutability": "view",
-"type": "function"
-}
+export const PUBABI=[
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "addToBalance",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_entryFee",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_potSize",
+				"type": "uint256"
+			}
+		],
+		"name": "createPublicEnvelope",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "envelopeId",
+				"type": "uint256"
+			}
+		],
+		"name": "distributeRewards",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "envelopeId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "participant",
+				"type": "address"
+			}
+		],
+		"name": "EnteredEnvelope",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "envelopeId",
+				"type": "uint256"
+			}
+		],
+		"name": "enterEnvelope",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "FundAdded",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "FundsWithdrawn",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "envelopeId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "creator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "entryFee",
+				"type": "uint256"
+			}
+		],
+		"name": "PublicEnvelopeCreated",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "envelopeId",
+				"type": "uint256"
+			}
+		],
+		"name": "PublicEnvelopeDistributed",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "user",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "subtractFromBalance",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "CREATOR_REWARD_PERCENTAGE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "currentEnvelopeId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "FEE_PERCENTAGE",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getAllEnvelopes",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "string[]",
+				"name": "",
+				"type": "string[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "loyaltyPoints",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "publicEnvelopes",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "creator",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "potSize",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "entryFee",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "creatorReward",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "userBalances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ]
